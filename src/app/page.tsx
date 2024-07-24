8ab1d59881b5f6684313698e17cd559c230069b1
kun-lex <akinola.funmi.2017@gmail.com>
2024-07-24 09:52:02 +0100

    update background of login page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,9 +4,20 @@ import Dashboard from "@/componets/Dashboard/Dashboard";
   import { AuthProvider } from "../context/AuthContext";
   import { CookiesProvider } from 'react-cookie'
   import ProtectedRoute from "@/componets/Auth";
+import { useEffect, useState } from "react";
  // import Layout from "./layout";
 
 export default function Home() {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    // Simulate loading delay (e.g., fetching data)
+    const timer = setTimeout(() => {
+      setLoading(false);
+    }, 1000); // Adjust the delay as needed
+
+    return () => clearTimeout(timer);
+  }, []);
   return (
    <CookiesProvider>
       <AuthProvider>
--- src/utils/hooks/useAuth.ts
+++ src/utils/hooks/useAuth.ts
@@ -1,21 +1,25 @@
 import { useUser } from "./useUser";
-// import config from "@/utils/config";
 import axios from "axios";
 import { AuthResponse, TLogin, TRegister } from "../types/auth";
 import useCookie from "./useCookies";
 
 const API_URL = 'https://ninemileon-backend-7ip8.onrender.com';
 
+const handleAxiosError = (err: any) => {
+  if (err && err?.response && err.response?.data) {
+    throw { ...err.response.data, success: false } as AuthResponse;
+  } else {
+    throw err;
+  }
+};
+
 export const useAuth = () => {
   const { user, addUser, removeUser } = useUser();
 
   const { getCookie } = useCookie();
 
-  const refresh = () => {
-    let existingUser = null;
-    const getFromCookie = async () => (existingUser = getCookie("user"));
-    getFromCookie();
-
+  const refreshUserFromCookie = async () => {
+    const existingUser = await getCookie("user");
     if (existingUser) {
       try {
         addUser(JSON.parse(existingUser));
@@ -26,36 +30,28 @@ export const useAuth = () => {
   };
 
   const register = async (creds: TRegister) => {
-    return await axios
-      .post(`${API_URL}auth/register`, creds)
-      .then((res) => {
-        if (res.data?.data && res.data.data?.token) addUser(res.data.data);
-        return res.data as AuthResponse;
-      })
-      .catch((err) => {
-        if (err && err?.response && err.response?.data)
-          return { ...err.response.data, success: false } as AuthResponse;
-        else return err as AuthResponse;
-      });
+    try {
+      const res = await axios.post(`${API_URL}auth/register`, creds);
+      if (res.data?.data && res.data.data?.token) addUser(res.data.data);
+      return res.data as AuthResponse;
+    } catch (err) {
+      handleAxiosError(err);
+    }
   };
 
   const login = async (creds: TLogin) => {
-    return await axios
-      .post(`${API_URL}auth/login`, creds)
-      .then((res) => {
-        if (res.data?.data && res.data.data?.token) addUser(res.data.data);
-        return res.data as AuthResponse;
-      })
-      .catch((err) => {
-        if (err && err?.response && err.response?.data)
-          return { ...err.response.data, success: false } as AuthResponse;
-        else return err as AuthResponse;
-      });
+    try {
+      const res = await axios.post(`${API_URL}auth/login`, creds);
+      if (res.data?.data && res.data.data?.token) addUser(res.data.data);
+      return res.data as AuthResponse;
+    } catch (err) {
+      handleAxiosError(err);
+    }
   };
 
   const logout = () => {
     removeUser();
   };
 
-  return { user, login, register, logout, refresh };
+  return { user, login, register, logout, refreshUserFromCookie };
 };
